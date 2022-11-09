db52dfee8fef26a5eb7f066175d4a84bdc4a9a18
FalahIsnain <1911016210008@mhs.ulm.ac.id>
2022-11-09 12:21:31 +0800

    edit arsip dashboard

--- app/Views/Arsip/dashboardarsip.php
+++ app/Views/Arsip/dashboardarsip.php
@@ -6,31 +6,56 @@
     <a href="<?= base_url('/Arsip') ?>">
         <button class="btn btn-primary mt-2 mb-2" type="submit"><i class="fas fa-plus"></i> Tambah Arsip</button>
     </a>
-    <?php $i = 1 ?>
-    <?php foreach ($arsip as $sm) : ?>
-        <div class="accordion" id="accordionExample">
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="heading<?= $i ?>">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $i ?>" aria-expanded="true" aria-controls="collapse<?= $i ?>">
-                        <?= $sm['nama_arsip'] ?>
-                    </button>
-                </h2>
-                <div id="collapse<?= $i ?>" class="accordion-collapse collapse" aria-labelledby="heading<?= $i ?>" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
-                        <!-- 16:9 aspect ratio -->
-                        <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" width="1220" height="400" src="asset/pdf/<?= $sm['file_arsip'] ?>"></iframe>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-        </div>
-        <?php $i = $i + 1 ?>
-    <?php endforeach; ?>
+    <div class="card-body">
+        <table id="datatablesSimple" class="table table-hover">
+            <thead>
+                <tr style="background-color:#A8876F;color:white ;">
+                    <th>Nama Dokumen</th>
+                    <th>Jenis Dokumen</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <th>Nama Dokumen</th>
+                    <th>Jenis Dokumen</th>
+                </tr>
+            </tfoot>
+            <tbody>
+                <?php $i = 1 ?>
+                <?php foreach ($arsip as $sm) : ?>
+                    <tr>
+                        <td>
+                            <div class="accordion" id="accordionExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="heading<?= $i ?>">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?= $i ?>" aria-expanded="true" aria-controls="collapse<?= $i ?>">
+                                            <?= $sm['nama_arsip'] ?>
+                                        </button>
+                                    </h2>
+                                    <div id="collapse<?= $i ?>" class="accordion-collapse collapse" aria-labelledby="heading<?= $i ?>" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <!-- 16:9 aspect ratio -->
+                                            <div class="embed-responsive embed-responsive-16by9">
+                                                <iframe class="embed-responsive-item" width="900" height="400" src="asset/pdf/<?= $sm['file_arsip'] ?>"></iframe>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </td>
+
+                        <td><?= $sm['nama_jenis'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
 
 </div>
-<!-- <?= 'asset/pdf/' . $sm['file_arsip'] ?> -->
+
 
 
 <?= $this->endSection(); ?>
